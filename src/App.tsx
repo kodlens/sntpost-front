1f9da3916dba7046acf5a3bb18b46c5365759bfe
etiennewayne <etiennewayne@gmail.com>
2025-12-09 10:10:12 +0800

    update react version

--- src/App.tsx
+++ src/App.tsx
@@ -14,43 +14,46 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import { config } from './config/config';
 import GlobalLoader from './components/GlobalLoader';
+import FourOFour from './pages/404/404';
 
 const siteKey = config.captchaSiteKey
 
 function App() {
-  const queryClient = new QueryClient()
+    const queryClient = new QueryClient()
 
-  return (
-    <GoogleReCaptchaProvider reCaptchaKey={siteKey}>
-      <QueryClientProvider client={queryClient}>
-        <GlobalLoader />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
+    return (
+        <GoogleReCaptchaProvider reCaptchaKey={siteKey}>
+            <QueryClientProvider client={queryClient}>
+                <GlobalLoader />
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
 
-          <Route //responsible for routing FrontEnd UI (eshen, code tracing)
-            path="/dost"
-            element={<MainLayout />}>
-            <Route path=":slug" element={<SingleArticle />} />
-          </Route>
+                    <Route //responsible for routing FrontEnd UI (eshen, code tracing)
+                        path="/dost"
+                        element={<MainLayout />}>
+                        <Route path=":slug" element={<SingleArticle />} />
+                    </Route>
 
-          <Route path="/category" element={<MainLayout />} >
-            <Route path=":slug" element={<ArticlesByCategory />} key="category-route"/>
-          </Route>
+                    <Route path="/category" element={<MainLayout />} >
+                        <Route path=":slug" element={<ArticlesByCategory />} key="category-route" />
+                    </Route>
 
-          <Route path="/magazines-all" element={<MagazinesAll />} />
-          <Route path="/search/:search" element={<SearchResult />} />
+                    <Route path="/magazines-all" element={<MagazinesAll />} />
+                    <Route path="/search/:search" element={<SearchResult />} />
 
-          <Route path="/magazines" element={<MainLayout />} >
-            <Route path="flipbook/:slug" element={<MagazinePDFFlipBook />} key="magazine-route"/>
-          </Route>
+                    <Route path="/magazines" element={<MainLayout />} >
+                        <Route path="flipbook/:slug" element={<MagazinePDFFlipBook />} key="magazine-route" />
+                    </Route>
 
-          <Route element={<MainLayout />} >
-            <Route path="/archives" element={<ArchiveIndex />} />
-          </Route>
-        </Routes>
-      </QueryClientProvider>
-    </GoogleReCaptchaProvider>
-  )
+                    <Route element={<MainLayout />} >
+                        <Route path="/archives" element={<ArchiveIndex />} />
+                    </Route>
+
+                    <Route path="*" element={<FourOFour />} />
+                </Routes>
+            </QueryClientProvider>
+        </GoogleReCaptchaProvider>
+    )
 }
 
 export default App
